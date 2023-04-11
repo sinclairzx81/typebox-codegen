8c61d627c46c9706e03a06ba7fcb1624d173b1ae
Pierre Dahmani <hi@pierre-dev.com>
2023-04-11 23:44:56 +0200

    adds test with depth1 and 2 for types and interfaces. TODO: for interfaces we get the warning 'Unhandled: EmptyStatement.'   - TODO: figure out why we get this warning and fix it( happens for interfaces only)   - TODO: ask why we log instead of throw for unhandled cases

--- __tests__/indexedAccessTypes.ts
+++ __tests__/indexedAccessTypes.ts
@@ -2,42 +2,138 @@ import { expectEqualIgnoreFormatting } from "./utils";
 import { TypeScriptToTypeBox } from "../src/typescript-to-typebox";
 
 describe("IndexedAccessTypes - Typescript to Typebox", () => {
-  it("Interface", async () => {
+  it("Interface depth 1", async () => {
     const tsInput = `
-export interface Vector {
-  x: number;
-  y: number;
-  z: number;
-}`;
-    const expectedTypeboxOutput = `import { Type, Static } from '@sinclair/typebox'
+      export interface Address {
+        street: string;
+        test: {
+          a: string;
+        };
+      };
 
-export type Vector = Static<typeof Vector>
-export const Vector = Type.Object({
-  x: Type.Number(),
-  y: Type.Number(),
-  z: Type.Number()
-})
-`;
+      export interface Address2 {
+        street2: Address["test"];
+      };
+      `;
+    const expectedTypeboxOutput = `
+      import { Type, Static } from '@sinclair/typebox';
+
+      export type Address = Static<typeof Address>;
+      export const Address = Type.Object({
+        street: Type.String(),
+        test: Type.Object({
+          a: Type.String()
+        })
+      });
+
+      export type Address2 = Static<typeof Address2>;
+      export const Address2 = Type.Object({
+        street2: Type.Object({
+          a: Type.String()
+        })
+      });
+      `;
     const output = TypeScriptToTypeBox.Generate(tsInput);
     expectEqualIgnoreFormatting(output, expectedTypeboxOutput);
   });
 
-  it("Type", async () => {
+  it("Interface depth 2", async () => {
     const tsInput = `
-export type Vector {
-  x: number;
-  y: number;
-  z: number;
-}`;
-    const expectedTypeboxOutput = `import { Type, Static } from '@sinclair/typebox'
+      export interface Address {
+        street: string;
+        test: {
+          a: string;
+        };
+      };
 
-export type Vector = Static<typeof Vector>
-export const Vector = Type.Object({
-  x: Type.Number(),
-  y: Type.Number(),
-  z: Type.Number()
-})
-`;
+      export interface Address2 {
+        street2: Address["test"]["a"];
+      };
+      `;
+    const expectedTypeboxOutput = `
+      import { Type, Static } from '@sinclair/typebox'
+
+      export type Address = Static<typeof Address>
+      export const Address = Type.Object({
+        street: Type.String(),
+        test: Type.Object({
+          a: Type.String()
+        })
+      })
+
+      export type Address2 = Static<typeof Address2>
+      export const Address2 = Type.Object({
+        street2: Type.String()
+      })
+      `;
+    const output = TypeScriptToTypeBox.Generate(tsInput);
+    expectEqualIgnoreFormatting(output, expectedTypeboxOutput);
+  });
+
+  it("Type depth 1", async () => {
+    const tsInput = `
+      export type Address = {
+        street: string;
+        test: {
+          a: string;
+        };
+      };
+
+      export type Address2 = {
+        street2: Address["test"];
+      };
+      `;
+    const expectedTypeboxOutput = `
+      import { Type, Static } from '@sinclair/typebox'
+
+      export type Address = Static<typeof Address>
+      export const Address = Type.Object({
+        street: Type.String(),
+        test: Type.Object({
+          a: Type.String()
+        })
+      })
+
+      export type Address2 = Static<typeof Address2>
+      export const Address2 = Type.Object({
+        street2: Type.Object({
+          a: Type.String()
+        })
+      })
+      `;
+    const output = TypeScriptToTypeBox.Generate(tsInput);
+    expectEqualIgnoreFormatting(output, expectedTypeboxOutput);
+  });
+
+  it("Type depth 2", async () => {
+    const tsInput = `
+      export type Address = {
+        street: string;
+        test: {
+          a: string;
+        };
+      };
+
+      export type Address2 = {
+        street2: Address["test"]["a"];
+      };
+      `;
+    const expectedTypeboxOutput = `
+      import { Type, Static } from '@sinclair/typebox'
+
+      export type Address = Static<typeof Address>
+      export const Address = Type.Object({
+        street: Type.String(),
+        test: Type.Object({
+          a: Type.String()
+        })
+      })
+
+      export type Address2 = Static<typeof Address2>
+      export const Address2 = Type.Object({
+        street2: Type.String()
+      })
+      `;
     const output = TypeScriptToTypeBox.Generate(tsInput);
     expectEqualIgnoreFormatting(output, expectedTypeboxOutput);
   });
